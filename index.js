0c94694df076c8632ce6810c256d3126acb266f3
mohammed anas <anasanu99950.com>
2026-01-14 13:53:55 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express=require('express')
+require('dotenv').config()
+const app=express()
+const port=process.env.PORT;
+const database=process.env.DATABASE_URL
+const dbName = process.env.DATABASE_NAME;
+
+
+app.get('/',(req,res)=>{
+res.send('Hello World')
+})
+app.get('/get',(req,res)=>{
+    res.send(`Database url:${database}, database name: ${dbName}`)
+})
+
+app.listen(port, function(){
+    console.log(`app running port ${port}`);
+})
